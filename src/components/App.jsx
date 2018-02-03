a50271a45e3a1873abdb159be22ce693db92e7e4
Gareth Sanashee <sanashee05@hotmail.com>
2018-02-03 21:36:03 +0000

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addReminder } from '../actions';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder(){
+    this.props.addReminder(this.state.text);
+  }
+
+  renderReminders() { 
+    const { reminders } = this.props; 
+    return (
+      <ul className="list-group col-sm-4">
+      {
+        reminders.map(reminder => {
+          return(
+            <li key={reminder.id} className="list-group-item">
+            <div>{reminder.text}</div> {/* LIST ITEMS */}
+            </li>
+          )
+        })
+      }
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          Reminder Pro Bro
+        </div>
+        <div className="form-inline reminder-form">
+          <div className="form-group">
+            <input className="form-control" placeholder="I have to.."
+            onChange={event => this.setState({text: event.target.value})}
+             />
+          </div>
+          <button type="button" className="btn btn-success"
+          onClick={() => this.addReminder()}
+          >
+            Add Reminder 
+          </button>
+        </div>
+        { this.renderReminders () } {/* PLACEMENT OF LIST ITEMS */}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  }
+}
+
+export default connect(mapStateToProps, {addReminder}) (App);
